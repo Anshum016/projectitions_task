a22f5134cc709ef8f5ab53f92a79f4eece0c3da5
SuraniHarsh <harshsurani00@gmail.com>
2024-01-03 10:20:32 +0530

    nav problem solved

--- src/components/ui/wires.tsx
+++ src/components/ui/wires.tsx
@@ -2,28 +2,21 @@ import React from "react";
 
 const Wires = () => {
   return (
-    <div className="fixed inset-0 z-20">
-      <div className="relative">
-        <div className="absolute left-0 top-0">
-          <img src="img/wire-1.png" alt="" />
-        </div>
-      </div>
-      <div className="relative">
-        <div className="absolute top-0 right-0">
-          <img src="img/wire-2.png" alt="" />
-        </div>
+    <div className="z-20 relative">
+      <div className="fixed top-0 left-0 h-64 w-64">
+        <img src="img/wire-1.png" alt="" />
       </div>
 
-      <div className="absolute left-0 bottom-0">
-        <div className="relative">
-          <img src="img/wire-3.png" alt="" />
-        </div>
+      <div className="fixed top-0 right-0 h-64 w-64">
+        <img src="img/wire-2.png" alt="" />
       </div>
 
-      <div className="absolute right-0 bottom-0">
-        <div className="relative">
-          <img src="img/wire-4.png" alt="" />
-        </div>
+      <div className="fixed bottom-0 left-0 ">
+        <img src="img/wire-3.png" alt="" />
+      </div>
+
+      <div className="fixed bottom-0 right-0">
+        <img src="img/wire-4.png" alt="" />
       </div>
     </div>
   );
